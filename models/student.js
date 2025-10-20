6b834ff3dee2541c7847c8ea22a91013f7663c14
mooooataz <mooooataz@example.com>
2025-10-20 16:04:39 +0300

    models

---
+++ models/student.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const StudentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.types.ObjectId,
+    ref: "Student",
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  class: {
+    type: String,
+    required: true,
+  },
+  documents: {
+    type: [String],
+    required: true,
+  },
+});
+const Student = mongoose.model(Student, StudentSchema);
+module.exports = Student;
